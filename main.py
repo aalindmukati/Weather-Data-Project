b0b2e9ed333516bfc5a08a17d58a0449021446e3
Aalind Mukati <aalindmukati@gmail.com>
2026-06-17 16:45:32 +0530

    experimenting with some things

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import requests as req
-import matplotlib as mat
-import seaborn
+import matplotlib.pyplot as mat
+import seaborn as sns
 
 
 df = pd.read_csv("Data.csv")
+# print(df)
+
+mat.figure(figsize=(10,6))
+
+s1 = sns.scatterplot(data=df,x='Date',y='Temperature')
+print(s1)
+
+mat.title('first data')
+mat.yscale('Temperature')
+mat.xscale('Date')
+
+mat.tight_layout()
+mat.show()
